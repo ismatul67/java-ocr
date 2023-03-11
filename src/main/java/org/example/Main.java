ce956e5a7e5363f8cf71af25c50d46564df7318a
RDivary <rnoor@fintax.id>
2023-03-11 11:55:36 +0700

    commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -14,6 +14,11 @@ import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 
 public class Main {
+
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_BLUE = "\u001B[34m";
+    public static final String ANSI_RESET = "\u001B[0m";
+
     public static void main(String[] args) throws IOException {
         String filePath = "https://drive.google.com/file/d/1sQMokp53qx4nvd4JUb8rsnmr5E7Kf4tX/view";
         URL url = new URL(filePath);
@@ -21,23 +26,41 @@ public class Main {
                 .openConnection();
         connection.setDoOutput(true);
         connection.setRequestMethod("GET");
-//        connection
-//                .setRequestProperty("Authorization",
-//                        "OAuth "+accessToken);
-//
-//        String docText = convertStreamToString(connection.getInputStream());
-        File file = new File("/Users/ismatulmaula/Downloads/BUKTIPERMANEN_SISWA_0057386892.jpeg");
+
+        File file = new File("D:/Download/motivation-quote-1.jpg");
 //        Files.copy(connection.getInputStream(), file.toPath(), StandardCopyOption.REPLACE_EXISTING);
         Tesseract tesseract = new Tesseract();
         try {
 
+            tesseract.setLanguage("eng");
+            tesseract.setDatapath("D:/Download/tessdata");
+
+
 //            tesseract.setDatapath(filePath);
+
             String text = tesseract.doOCR(file);
-            System.out.print(text);
+//            System.out.println(ANSI_RED + text + ANSI_RESET);
+//            System.out.println(ANSI_BLUE + text + ANSI_RESET);
+//
+//            System.out.print(text);
+            print(text);
         }
         catch (TesseractException e) {
             e.printStackTrace();
         }
 
+    }
+
+    private static void print(String text){
+        String[] arr = text.split(" ");
+        StringBuilder sb = new StringBuilder();
+        for (String word : arr){
+            if (word.contains("o") || word.contains("O")){
+                sb.append(ANSI_BLUE).append(word).append(ANSI_RESET).append(" ");
+            } else {
+                sb.append(word).append(" ");
+            }
+        }
+        System.out.println(sb);
     }
 }
